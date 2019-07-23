dc7aa0193dc5df8e1649e4fefdc70c97a529039e
bradford-james <bwettig@gmail.com>
2019-07-22 23:13:57 -0500

    Changed dependancy/API for retrieving random quotes

--- utils/getQuotes.js
+++ utils/getQuotes.js
@@ -1,8 +1,36 @@
 const axios = require("axios");
+const htmlToText = require("html-to-text");
+
+const genNumSeq = () => {
+  const num = Math.floor(Math.random() * 200);
+  let primary = 0;
+  let secondary = 0;
+  let inc = 1;
+  for (i = 1; i < 50; i++) {
+    primary++;
+    secondary = num - inc;
+    inc = inc + i;
+    if (inc > num) break;
+  }
+  return [primary, secondary];
+};
 
 module.exports = async category => {
-  const results = await axios.get("https://quotes.rest/qod");
+  const numSeq = genNumSeq();
 
-  return `${results.data.contents.quotes[0].quote}
-      -${results.data.contents.quotes[0].author}`;
+  const results = await axios.get(
+    `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${
+      numSeq[0]
+    }`
+  );
+  const index = numSeq[1];
+  const quote = htmlToText.fromString(results.data[index].content, {
+    wordwrap: 60
+  });
+  const author = htmlToText.fromString(results.data[index].title);
+
+  return `
+  ${quote}
+    -${author}
+      `;
 };
